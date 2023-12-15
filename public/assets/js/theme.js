6c0ff45df19ebb375a98d9ea6e4697cf3515182b
Mishab <mishabmsb91@gmail.com>
2023-12-15 09:59:14 +0530

    font changed

--- public/assets/js/theme.js
+++ public/assets/js/theme.js
@@ -18,9 +18,9 @@
         wind.on('scroll', function () {
             var scroll = wind.scrollTop();
             if (scroll < 100) {
-                sticky.removeClass('sticky-nav');
+                sticky.removeClass('');
             } else {
-                sticky.addClass('sticky-nav');
+                sticky.addClass('');
             }
         });
 
@@ -612,29 +612,29 @@ $(document).ready(function (o) {
 });
 
 var dropdown = document.querySelectorAll('.dropdown');
-		var dropdownArray = Array.prototype.slice.call(dropdown, 0);
-		dropdownArray.forEach(function (el) {
-			var button = el.querySelector('a[data-toggle="dropdown"]'),
-				menu = el.querySelector('.dropdown-menu'),
-				arrow = button.querySelector('i.icon-arrow');
+var dropdownArray = Array.prototype.slice.call(dropdown, 0);
+dropdownArray.forEach(function (el) {
+    var button = el.querySelector('a[data-toggle="dropdown"]'),
+        menu = el.querySelector('.dropdown-menu'),
+        arrow = button.querySelector('i.icon-arrow');
 
-			button.onclick = function (event) {
-				if (!menu.hasClass('show')) {
-					menu.classList.add('show');
-					menu.classList.remove('hide');
-					arrow.classList.add('open');
-					arrow.classList.remove('close');
-					event.preventDefault();
-				} else {
-					menu.classList.remove('show');
-					menu.classList.add('hide');
-					arrow.classList.remove('open');
-					arrow.classList.add('close');
-					event.preventDefault();
-				}
-			};
-		})
+    button.onclick = function (event) {
+        if (!menu.hasClass('show')) {
+            menu.classList.add('show');
+            menu.classList.remove('hide');
+            arrow.classList.add('open');
+            arrow.classList.remove('close');
+            event.preventDefault();
+        } else {
+            menu.classList.remove('show');
+            menu.classList.add('hide');
+            arrow.classList.remove('open');
+            arrow.classList.add('close');
+            event.preventDefault();
+        }
+    };
+})
 
-		Element.prototype.hasClass = function (className) {
-			return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
-		};
+Element.prototype.hasClass = function (className) {
+    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+};
